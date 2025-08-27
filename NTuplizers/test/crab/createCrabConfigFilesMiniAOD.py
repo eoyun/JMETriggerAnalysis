b31a51f25d8074f28ef9cffc53cc176482a05fa9
S. Lehti <sami.lehti@cern.ch>
2025-08-27 16:02:18 +0200

    Fixed Muon 2025E samples

--- NTuplizers/test/crab/createCrabConfigFilesMiniAOD.py
+++ NTuplizers/test/crab/createCrabConfigFilesMiniAOD.py
@@ -63,14 +63,14 @@ samples_muons = {
   'Muon0_Run2025C1':["/Muon0/Run2025C-PromptReco-v1/MINIAOD","","Collisions25/DCSOnly_JSONS/dailyDCSOnlyJSON/Collisions25_13p6TeV_Latest.json","150X_dataRun3_Prompt_v1"],
   'Muon0_Run2025C2':["/Muon0/Run2025C-PromptReco-v2/MINIAOD","","Collisions25/DCSOnly_JSONS/dailyDCSOnlyJSON/Collisions25_13p6TeV_Latest.json","150X_dataRun3_Prompt_v1"],
   'Muon0_Run2025D':["/Muon0/Run2025D-PromptReco-v1/MINIAOD","","Collisions25/DCSOnly_JSONS/dailyDCSOnlyJSON/Collisions25_13p6TeV_Latest.json","150X_dataRun3_Prompt_v1"],
-  'Muon0_Run2025E':["/Muon0/Run2025D-PromptReco-v1/MINIAOD","","Collisions25/DCSOnly_JSONS/dailyDCSOnlyJSON/Collisions25_13p6TeV_Latest.json","150X_dataRun3_Prompt_v1"],
+  'Muon0_Run2025E':["/Muon0/Run2025E-PromptReco-v1/MINIAOD","","Collisions25/DCSOnly_JSONS/dailyDCSOnlyJSON/Collisions25_13p6TeV_Latest.json","150X_dataRun3_Prompt_v1"],
 
   #2025 Muon1
   'Muon1_Run2025B':["/Muon1/Run2025B-PromptReco-v1/MINIAOD","","Collisions25/DCSOnly_JSONS/dailyDCSOnlyJSON/Collisions25_13p6TeV_Latest.json","150X_dataRun3_Prompt_v1"],
   'Muon1_Run2025C1':["/Muon1/Run2025C-PromptReco-v1/MINIAOD","","Collisions25/DCSOnly_JSONS/dailyDCSOnlyJSON/Collisions25_13p6TeV_Latest.json","150X_dataRun3_Prompt_v1"],
   'Muon1_Run2025C2':["/Muon1/Run2025C-PromptReco-v2/MINIAOD","","Collisions25/DCSOnly_JSONS/dailyDCSOnlyJSON/Collisions25_13p6TeV_Latest.json","150X_dataRun3_Prompt_v1"],
   'Muon1_Run2025D':["/Muon1/Run2025D-PromptReco-v1/MINIAOD","","Collisions25/DCSOnly_JSONS/dailyDCSOnlyJSON/Collisions25_13p6TeV_Latest.json","150X_dataRun3_Prompt_v1"],
-  'Muon1_Run2025E':["/Muon1/Run2025D-PromptReco-v1/MINIAOD","","Collisions25/DCSOnly_JSONS/dailyDCSOnlyJSON/Collisions25_13p6TeV_Latest.json","150X_dataRun3_Prompt_v1"],
+  'Muon1_Run2025E':["/Muon1/Run2025E-PromptReco-v1/MINIAOD","","Collisions25/DCSOnly_JSONS/dailyDCSOnlyJSON/Collisions25_13p6TeV_Latest.json","150X_dataRun3_Prompt_v1"],
 }
 
 samples = {
