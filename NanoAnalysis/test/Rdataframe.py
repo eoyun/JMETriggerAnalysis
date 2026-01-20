63fb13b956df6918a549053a6f18cfc8dabe6bd1
eoyun <84005323+eoyun@users.noreply.github.com>
2026-01-20 12:30:35 +0900

    Update NanoAnalysis script docs and cleanup

--- NanoAnalysis/test/Rdataframe.py
+++ NanoAnalysis/test/Rdataframe.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 
-import datetime
-import numpy as np
 import ROOT
 import argparse
-import os
 import LumiMask
 
 parser = argparse.ArgumentParser()
@@ -15,49 +12,23 @@ parser.add_argument("-e", "--era", dest="era", action="store", default="2024")
 args = parser.parse_args()
 
 ROOT.ROOT.EnableImplicitMT()
-def declare_struc():
-    if not hasattr(ROOT, 'RDFAddArray'):
-        ROOT.gInterpreter.Declare('''
-        ROOT::RDF::RNode RDFAddArray(ROOT::RDF::RNode df, ROOT::RVec<double> &v, const std::string &name) 
-        {
-            return df.Define(name, [&](unsigned c) { return v[c]; }, {"counter"});
-        }
-        unsigned counter = 0;
-
-        ''')
-
-def dphi(phi):
-    """Calculates delta phi between objects"""
-    x = np.abs(phi[1] - phi[0])
-    sign = x<=np.pi
-    dphi = sign* x + ~sign * (2*np.pi - x)
-    return dphi
-
-
 
 def analysis(filename_idx,idx,outputdir):
-    declare_struc()
+    # [1] Remote input path (XRootD)
     inputfile = "root://xrootd-cms.infn.it/"+filename_idx
-    #df = ROOT.RDataFrame("Events", "DYJetsToLL_M_50_2022.root")
-    #df = ROOT.RDataFrame("Events","root://xrootd-cms.infn.it//store/data/Run2023B/JetMET0/NANOAOD/22Sep2023-v1/2540000/060eed3c-d114-4135-b3f6-2cc6c8cf4c19.root")
-    #df = ROOT.RDataFrame("Events","root://xrootd-cms.infn.it//store/data/Run2023D/JetMET0/NANOAOD/22Sep2023_v1-v1/2530000/90b4ce31-2fb1-4822-8ca4-aef2c302761d.root")
     print(inputfile)
     df = ROOT.RDataFrame("Events",inputfile)
-    #df = ROOT.RDataFrame("Events","root://xrootd-cms.infn.it//store/mc/Run3Winter23NanoAOD/QCD_PT-15to7000_TuneCP5_13p6TeV_pythia8/NANOAODSIM/126X_mcRun3_2023_forPU65_v1-v1/2540000/a1c3034d-ce5a-4d4c-9ee6-939b6c04edb5.root")
-    df = df.Filter("nJet >= 2", "Events with exactly two muons")
-    #df = df.Filter("Muon_charge[0] + Muon_charge[1] == 0", "Muons with opposite charge")
-
-    #df_dimuon = df.Define("Dimuon_mass", "InvariantMass(Muon_pt, Muon_eta, Muon_phi, Muon_mass)")
+    # [2] Basic jet selections and kinematics
+    df = df.Filter("nJet >= 2", "Events with at least two jets")
     df = df.Define("Lead_jet", "Jet_pt[0]")
     df = df.Define("Sub_jet", "Jet_pt[1]")
     df = df.Define("Lead_jet_phi", "Jet_phi[0]")
     df = df.Define("Sub_jet_phi", "Jet_phi[1]")
     df = df.Define("Lead_jet_eta", "Jet_eta[0]")
     df = df.Define("Sub_jet_eta", "Jet_eta[1]")
-    #print(np.array(df_lead_jet.Take['float']("Lead_jet").GetValue()))
-    #print(type(ROOT.vector['float'](np.zeros(10))))
     ROOT.gInterpreter.Declare(
             """
+            // [3] Delta-phi helper for dijet selections
             double dphi(double phi1, double phi2){
                 float x  = abs(phi1-phi2);
                 if (x>M_PI)return 2*M_PI - x;
@@ -67,6 +38,7 @@ def analysis(filename_idx,idx,outputdir):
             )
     ROOT.gInterpreter.Declare(
             """
+            // [4] Match each offline jet to the nearest HLT jet within deltaR
             vector<int> match_hlt_offline ( ROOT::VecOps::RVec<float> hlt_eta,ROOT::VecOps::RVec<float> hlt_phi,ROOT::VecOps::RVec<float> off_eta,ROOT::VecOps::RVec<float> off_phi){
                 vector<int> hlt_matched;
                 int off_index;
@@ -98,6 +70,7 @@ def analysis(filename_idx,idx,outputdir):
             )
     ROOT.gInterpreter.Declare(
             """
+            // [5] Safe indexing for matched HLT arrays
             float get_or_default(ROOT::VecOps::RVec<float> values, int index, float fallback) {
                 if (index < 0 || index >= static_cast<int>(values.size())) {
                     return fallback;
@@ -112,31 +85,9 @@ def analysis(filename_idx,idx,outputdir):
             }
             """
             )
-    #df_delta_phi = df.Define("Delta_phi","double x = Jet_phi[0]- Jet_phi[1]; return x > TMath::pi ? 2 * TMath::pi - x : x")
     df = df.Define("Delta_phi","dphi(Jet_phi[0],Jet_phi[1])")
     df = df.Define("alpha","nJet==2 ? 0 : 2 * Jet_pt[2]/(Jet_pt[0]+Jet_pt[1])")
-    #df = df.Define("Trig_obj_pt","TrigObj_pt[TrigObj_id == 1 && (TrigObj_filterBits % 64) /32  != 0 && (TrigObj_filterBits % 4096) / 2048 == 0]")
-    #df = df.Define("hlt_Jet_eta","TrigObj_eta[TrigObj_id == 1 && (TrigObj_filterBits % 64) /32  != 0 && (TrigObj_filterBits % 4096) / 2048 == 0]")
-    #df = df.Define("hlt_Jet_phi","TrigObj_phi[TrigObj_id == 1 && (TrigObj_filterBits % 64) /32  != 0 && (TrigObj_filterBits % 4096) / 2048 == 0]")
-    #df = df.Define("hlt_Jet_id","TrigObj_filterBits[TrigObj_id==1&& (TrigObj_filterBits % 64) /32  != 0&& (TrigObj_filterBits % 4096) / 2048 == 0]")
-    #df = df.Define("Trig_obj_pt","TrigObj_pt[TrigObj_id == 1 && (TrigObj_filterBits % 64) /32  != 0]")
-    #df = df.Define("hlt_Jet_eta","TrigObj_eta[TrigObj_id == 1 && (TrigObj_filterBits % 64) /32  != 0]")
-    #df = df.Define("hlt_Jet_phi","TrigObj_phi[TrigObj_id == 1 && (TrigObj_filterBits % 64) /32  != 0]")
-    #df = df.Define("hlt_Jet_id","TrigObj_filterBits[TrigObj_id==1&& (TrigObj_filterBits % 64) /32  != 0]")
-    bitmask_42 = 1 << 41
-    print(bitmask_42)
-    #df = df.Define("Trig_obj_pt",f"TrigObj_pt[TrigObj_id == 1 && (TrigObj_filterBits & {bitmask_42}) != 0]")
-    #df = df.Define("hlt_Jet_eta",f"TrigObj_eta[TrigObj_id == 1 && (TrigObj_filterBits & {bitmask_42}) != 0]")
-    #df = df.Define("hlt_Jet_phi",f"TrigObj_phi[TrigObj_id == 1 && (TrigObj_filterBits & {bitmask_42}) != 0]")
-    #df = df.Define("hlt_Jet_id",f"TrigObj_filterBits[TrigObj_id==1&& (TrigObj_filterBits & {bitmask_42})  != 0]")
-    #df = df.Define("Trig_obj_pt",f"TrigObj_pt[TrigObj_id == 1 && (TrigObj_filterBits & 2048) == 0]")
-    #df = df.Define("hlt_Jet_eta",f"TrigObj_eta[TrigObj_id == 1 && (TrigObj_filterBits & 2048) == 0]")
-    #df = df.Define("hlt_Jet_phi",f"TrigObj_phi[TrigObj_id == 1 && (TrigObj_filterBits & 2048) == 0]")
-    #df = df.Define("hlt_Jet_id",f"TrigObj_filterBits[TrigObj_id==1&& (TrigObj_filterBits & 2048) == 0]")
-    #df = df.Define("Trig_obj_pt",f"TrigObj_pt[TrigObj_id == 1 &&( (TrigObj_filterBits & 32) != 0 || (TrigObj_filterBits & 128) != 0 )]")
-    #df = df.Define("hlt_Jet_eta",f"TrigObj_eta[TrigObj_id == 1 &&( (TrigObj_filterBits & 32) != 0 || (TrigObj_filterBits & 128) != 0 )]")
-    #df = df.Define("hlt_Jet_phi",f"TrigObj_phi[TrigObj_id == 1 &&( (TrigObj_filterBits & 32) != 0 || (TrigObj_filterBits & 128) != 0 )]")
-    #df = df.Define("hlt_Jet_id",f"TrigObj_filterBits[TrigObj_id==1&&( (TrigObj_filterBits & 32) != 0 || (TrigObj_filterBits & 128) != 0 )]")
+    # [6] HLT jet collections (all jet-type trigger objects)
     df = df.Define("Trig_obj_pt","TrigObj_pt[TrigObj_id == 1 ]")
     df = df.Define("hlt_Jet_eta","TrigObj_eta[TrigObj_id == 1 ]")
     df = df.Define("hlt_Jet_phi","TrigObj_phi[TrigObj_id == 1 ]")
@@ -146,8 +97,7 @@ def analysis(filename_idx,idx,outputdir):
     df = df.Define("hlt_Jet_matched_id","get_or_default_int(hlt_Jet_id, Jet_hlt_matched_index[0], -1)")
     df = df.Define("hlt_Jet_matched_eta","get_or_default(hlt_Jet_eta, Jet_hlt_matched_index[0], -99.0)")
     df = df.Define("hlt_Jet_matched_phi","get_or_default(hlt_Jet_phi, Jet_hlt_matched_index[0], -99.0)")
-    #print(df.Take['vector<int>']("Jet_hlt_matched_index").GetValue())
-    #df_delta_phi = df.Define("Delta_phi","dphi(Jet_phi)")
+    # [7] Apply certified lumisections
     lumi_mask = LumiMask.lumimask(int(args.era))
     df = df.Filter(lambda run, lumi: lumi_mask.accept(run, lumi), ["run", "luminosityBlock"])
     df = df.Filter("Jet_hlt_matched_index[0]!=-1","") 
@@ -155,7 +105,6 @@ def analysis(filename_idx,idx,outputdir):
     df_after = df_after.Filter("alpha < 0.1","") 
     df_after = df_after.Filter("HLT_PFJet320","") 
     df = df.Filter("Lead_jet_eta < 1.3")
-    #df = df.Filter("L1_SingleJet200 || L1_SingleJet160er2p5 || L1_SingleJet180","")
     df_den_40  = df
     df_den_60  = df.Filter("HLT_PFJet40","")
     df_den_80  = df.Filter("HLT_PFJet60","")
@@ -177,9 +126,6 @@ def analysis(filename_idx,idx,outputdir):
     df_num_400 = df_den_400.Filter("hlt_Jet_off_matched_pt>400","") 
     df_num_450 = df_den_450.Filter("hlt_Jet_off_matched_pt>450","") 
     df_num_500 = df_den_500.Filter("hlt_Jet_off_matched_pt>500","") 
-    #df_500_cut = df_500.Filter("Lead_jet<400","") 
-    #df_500_cut_over = df_500.Filter("Lead_jet>500","") 
-
     df_den_500_cut = df_den_500.Filter("Lead_jet<200")
     df_num_500_cut = df_num_500.Filter("Lead_jet<200")
     
@@ -520,31 +466,6 @@ def analysis(filename_idx,idx,outputdir):
     _cut_num_500_histo_matched_phi = df_num_500_cut.Histo1D(("h_cut_num_500_hlt_matched_phi",";pT;a.u.",100,-3.2,3.2),"hlt_Jet_matched_phi")
     
     fOUT = ROOT.TFile.Open(outputdir+"/output_hist"+str(idx)+".root","RECREATE")
-    #df_den_40.Snapshot("newtree","./"+outputdir+"/output_40_den_"+str(idx)+".root")
-    #df_num_40.Snapshot("newtree","./"+outputdir+"/output_40_num_"+str(idx)+".root")
-    #df_den_60.Snapshot("newtree","./"+outputdir+"/output_60_den_"+str(idx)+".root")
-    #df_num_60.Snapshot("newtree","./"+outputdir+"/output_60_num_"+str(idx)+".root")
-    #df_den_80.Snapshot("newtree","./"+outputdir+"/output_80_den_"+str(idx)+".root")
-    #df_num_80.Snapshot("newtree","./"+outputdir+"/output_80_num_"+str(idx)+".root")
-    #df_den_140.Snapshot("newtree","./"+outputdir+"/output_140_den_"+str(idx)+".root")
-    #df_num_140.Snapshot("newtree","./"+outputdir+"/output_140_num_"+str(idx)+".root")
-    #df_den_200.Snapshot("newtree","./"+outputdir+"/output_200_den_"+str(idx)+".root")
-    #df_num_200.Snapshot("newtree","./"+outputdir+"/output_200_num_"+str(idx)+".root")
-    #df_den_260.Snapshot("newtree","./"+outputdir+"/output_260_den_"+str(idx)+".root")
-    #df_num_260.Snapshot("newtree","./"+outputdir+"/output_260_num_"+str(idx)+".root")
-    #df_den_320.Snapshot("newtree","./"+outputdir+"/output_320_den_"+str(idx)+".root")
-    #df_num_320.Snapshot("newtree","./"+outputdir+"/output_320_num_"+str(idx)+".root")
-    #df_den_400.Snapshot("newtree","./"+outputdir+"/output_400_den_"+str(idx)+".root")
-    #df_num_400.Snapshot("newtree","./"+outputdir+"/output_400_num_"+str(idx)+".root")
-    #df_den_450.Snapshot("newtree","./"+outputdir+"/output_450_den_"+str(idx)+".root")
-    #df_num_450.Snapshot("newtree","./"+outputdir+"/output_450_num_"+str(idx)+".root")
-    #df_den_500.Snapshot("newtree","./"+outputdir+"/output_500_den_"+str(idx)+".root")
-    #df_num_500.Snapshot("newtree","./"+outputdir+"/output_500_num_"+str(idx)+".root")
-    #days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-    #now = datetime.datetime.now()
-    #m = "produced: %s %s"%(days[now.weekday()],now)
-    #timestamp = ROOT.TNamed(m,"")
-    #timestamp.Write()
 
     histo.Write()
     histo_sub.Write()
@@ -899,5 +820,3 @@ if __name__ == "__main__":
         filelist.append(line)
     print(filelist[index])
     analysis(filelist[index],index,outputdir)
-
-
